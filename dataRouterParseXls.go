ff99152f35a1628e3bf5c186cd51d9301d952e6c
zhuxp <nimezhu@gmail.com>
2017-11-06 16:40:32 -0500

    xls sheet works in map[string][]string

--- dataRouterParseXls.go
+++ dataRouterParseXls.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"path"
 	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/nimezhu/netio"
 	"github.com/tealeg/xlsx"
@@ -43,7 +45,7 @@ func parseXls(uri string) ([]dataIndex, error) {
 		Id       string `xlsx:"0"`
 		Type     string `xlsx:"1"`
 		Nc       int    `xlsx:"2"`
-		Vc       int    `xlsx:"3"`
+		Vc       string `xlsx:"3"` //TODO VC is []ints
 		Preserve bool   `xlsx:"4"`
 	}
 	for _, row := range idxRows[1:] {
@@ -61,32 +63,60 @@ func parseXls(uri string) ([]dataIndex, error) {
 			k := a.Id
 			fmt.Println("init", k, format)
 
-			data := make(map[string]string)
+			data := make(map[string]interface{})
 			if a.Type == "map" {
+				vcs := strings.Split(a.Vc, ",")
+				vc, _ := strconv.Atoi(vcs[0])
 				for _, r := range vsheet.Rows[1:] {
-					v0 := r.Cells[a.Vc-1].String()
+					//v0 := r.Cells[a.Vc-1].String() //TODO vc is ints
+					v0 := r.Cells[vc-1].String()
 					k0 := r.Cells[a.Nc-1].String()
 					//m0.AddURI(v0, k0)
 					data[k0] = v0
 
 				}
 			} else {
-				for _, r := range vsheet.Rows[1:] {
-					id := r.Cells[a.Nc-1].String()
-					loc := r.Cells[a.Vc-1].String()
-					var uri string
-					if httpP.MatchString(loc) || httpsP.MatchString(loc) {
-						uri = loc
-						data[id] = uri
-					} else {
-						uri = path.Join(root, loc) //TODO
-						if _, err := os.Stat(uri); err == nil {
+				vcs := strings.Split(a.Vc, ",")
+				vc, _ := strconv.Atoi(vcs[0])
+				if len(vcs) == 1 {
+					for _, r := range vsheet.Rows[1:] {
+						id := r.Cells[a.Nc-1].String()
+						loc := r.Cells[vc-1].String() //TODO Vc To ints
+						var uri string
+						if httpP.MatchString(loc) || httpsP.MatchString(loc) {
+							uri = loc
 							data[id] = uri
 						} else {
-							log.Println("WARNING!!! cannot reading", uri, id)
+							uri = path.Join(root, loc) //TODO
+							if _, err := os.Stat(uri); err == nil {
+								data[id] = uri
+							} else {
+								log.Println("WARNING!!! cannot reading", uri, id)
+							}
 						}
 					}
-
+				} else {
+					for _, r := range vsheet.Rows[1:] {
+						id := r.Cells[a.Nc-1].String()
+						loc := r.Cells[vc-1].String() //TODO Vc To ints
+						vals := make([]string, len(vcs))
+						for i, k := range vcs {
+							v, _ := strconv.Atoi(k)
+							vals[i] = r.Cells[v-1].String()
+						}
+						var uri string
+						if httpP.MatchString(loc) || httpsP.MatchString(loc) {
+							data[id] = vals
+						} else {
+							uri = path.Join(root, loc) //TODO
+							vals[0] = uri
+							if _, err := os.Stat(uri); err == nil {
+								data[id] = vals
+							} else {
+								log.Println("WARNING!!! cannot reading", uri, id)
+							}
+						}
+					}
 				}
 			}
 			d := dataIndex{
