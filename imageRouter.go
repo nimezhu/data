816e74554bbdb12b907f4aab47de610302890b4a
zhuxp <nimezhu@gmail.com>
2017-11-06 11:49:58 -0500

    rm imageManager

--- imageRouter.go
+++ imageRouter.go
@@ -12,6 +12,31 @@ import (
 	"github.com/nimezhu/data/image"
 )
 
+func getRootDir(dirs []string) string {
+	r := ""
+	a := make([][]string, len(dirs))
+	for i, v := range dirs {
+		a[i] = strings.Split(v, "/") //TODO handle windows ???
+	}
+	for j, sign := 0, true; sign; j++ {
+		//v := a[0][j]
+		for i := 1; i < len(a); i++ {
+			if len(a[i]) < j {
+				sign = false
+				break
+			}
+			if a[i][j] != a[0][j] {
+				sign = false
+				break
+			}
+		}
+		if sign && len(a[0][j]) > 0 {
+			r += "/" + a[0][j]
+		}
+	}
+	return r
+}
+
 /*TODO implement DataRouter
  *
  */
