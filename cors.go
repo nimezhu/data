a9ad42fa6c06498483c8eb66e92aba1d4ee08d9e
zhuxp <nimezhu@gmail.com>
2018-06-13 10:57:01 -0400

    add CorsFactory

--- cors.go
+++ cors.go
@@ -10,3 +10,13 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+func CorsFactory(sites string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Do stuff here
+			w.Header().Set("Access-Control-Allow-Origin", sites)
+			// Call the next handler, which can be another middleware in the chain, or the final handler.
+			next.ServeHTTP(w, r)
+		})
+	}
+}
