20cd60a79ace162c5f64047b8ca9d5344a85016e
zhuxp <nimezhu@gmail.com>
2018-06-13 12:13:32 -0400

    add GsheetConfig

--- gsheetsAgent.go
+++ gsheetsAgent.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 type gAgent struct {
@@ -33,4 +34,16 @@ func (agent gAgent) tokenCacheFile() (string, error) {
 	os.MkdirAll(tokenCacheDir, 0700)
 	return filepath.Join(tokenCacheDir,
 		url.QueryEscape("gsheets.json")), nil
+}
+
+func GsheetConfig() *oauth2.Config {
+	b, err := Asset("client_secret.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	return config
 }
